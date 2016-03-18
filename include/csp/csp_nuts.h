b27482b4b46d334db64d55d22509cd6c7705c228
Magne Normann <magnealvnor@gmail.com>
2016-03-18 19:44:25 +0100

    Added csp_nuts_init() func and handling of MY_ADDR

--- include/csp/csp_nuts.h
+++ include/csp/csp_nuts.h
@@ -1,9 +1,10 @@
 /* This header file contains all csp information specific for NUTS */
 
-#define OBC_ADDR			0x1
+#define OBC_ADDR				0x1
+#define HOUSEKEEPER_RX_PORT		0x10
+#define TIMEKEEPER_RX_PORT		0x11
+#define TIMEKEEPER_RX_PORT_RDP	0x14
+#define EVENT_MAN_RX_PORT		0x12
+#define LOGGER_RX_PORT			0x13
 
-#define HOUSEKEEPER_RX_PORT	0x10
-#define TIMEKEEPER_RX_PORT	0x11
-#define TIMEKEEPER_RX_PORT_RDP 0x14
-#define EVENT_MAN_RX_PORT	0x12
-#define LOGGER_RX_PORT		0x13
+uint8_t csp_nuts_init(void);
---
+++ src/csp_nuts.c
@@ -0,0 +1,37 @@
+/*
+ * csp_nuts.c
+ *
+ * Created: 18.03.2016 18:56:12
+ *  Author: Normann
+ */ 
+
+#include "csp/csp.h"
+#include "csp/csp_nuts.h"
+#include "config/conf_csp.h"
+
+uint8_t csp_nuts_init(void){
+		/* Init buffer system with 10 packets of maximum 300 bytes each */
+	printf("Initialising CSP\r\n");
+	csp_buffer_init(10, 300);
+	
+	/* Init CSP with address MY_ADDR */
+	csp_init(MY_ADDR);
+
+	/* Start router task with 1000 word stack, OS task priority 1 */
+	csp_route_start_task(1000, 1);
+
+	/* Enable debug output from CSP */
+	printf("Debug enabed\r\n");
+	csp_debug_toggle_level(CSP_BUFFER);
+	csp_debug_toggle_level(CSP_PACKET);
+	csp_debug_toggle_level(CSP_PROTOCOL);
+
+	printf("Conn table\r\n");
+	csp_conn_print_table();
+
+	printf("Route table\r\n");
+	csp_route_print_table();
+
+	printf("Interfaces\r\n");
+	csp_route_print_interfaces();
+}
